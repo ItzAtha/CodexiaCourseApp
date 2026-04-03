133f255405ca41dbc409e4792967537138919b79
ItzAtha <atha.06dha@gmail.com>
2026-04-03 11:14:40 +0700

    Refactor AuthenticationGate to use ConsumerWidget and integrate auth user state with error handling and loading feedback

--- lib/features/auth/auth_router.dart
+++ lib/features/auth/auth_router.dart
@@ -1,16 +1,42 @@
+import 'package:codexia_course_learning/shared/providers/auth_user_notifier.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:toastification/toastification.dart';
 
 import 'views/opening_page.dart';
 import '../home/dashboard_page.dart';
 
-class AuthenticationGate extends StatelessWidget {
+class AuthenticationGate extends ConsumerWidget {
   final String _title;
 
   const AuthenticationGate({super.key, required String title}) : _title = title;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(authUserProvider, (previous, next) {
+      next.when(
+        data: (data) {
+          print("Successfully loaded profile: ${data.username}");
+        },
+        error: (error, stackTrace) {
+          print("Error loading profile: $error");
+          Toastification().show(
+            context: context,
+            title: Text("Couldn't load profile"),
+            type: ToastificationType.error,
+            style: ToastificationStyle.flat,
+            alignment: Alignment.bottomCenter,
+            autoCloseDuration: Duration(seconds: 2),
+            animationDuration: Duration(milliseconds: 500),
+          );
+        },
+        loading: () {
+          print("Loading profile...");
+        },
+      );
+    });
+
     return Scaffold(
       body: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
