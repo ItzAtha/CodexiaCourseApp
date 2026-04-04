23457111f170c3ed4fbc147b287cba932c73c74a
ItzAtha <atha.06dha@gmail.com>
2026-04-04 10:38:43 +0700

    Add UserAvatar class for managing user avatar data and serialization

---
+++ lib/shared/models/user_avatar.dart
@@ -0,0 +1,24 @@
+class UserAvatar {
+  String publicId;
+  String? avatarPath;
+
+  UserAvatar({required this.publicId, this.avatarPath});
+
+  factory UserAvatar.fromJson(Map<String, dynamic> json) {
+    return UserAvatar(
+      publicId: json['public_id'],
+      avatarPath: json['avatarPath'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'public_id': publicId, 'avatarPath': avatarPath};
+  }
+
+  UserAvatar copyWith({String? publicId, String? avatarPath}) {
+    return UserAvatar(
+      publicId: publicId ?? this.publicId,
+      avatarPath: avatarPath ?? this.avatarPath,
+    );
+  }
+}
