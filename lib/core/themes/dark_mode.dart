f505caec158e6188761dd79445286636da97128f
ItzAtha <atha.06dha@gmail.com>
2026-04-06 12:47:39 +0700

    Add light and dark theme configurations with Material 3 support

---
+++ lib/core/themes/dark_mode.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class DarkMode {
+  static ThemeData initialize() {
+    return ThemeData(
+      useMaterial3: true,
+      dividerColor: Colors.transparent,
+      scaffoldBackgroundColor: Color(0xFF212121),
+      iconTheme: IconThemeData(color: Color(0xCCF5F6FA)),
+      radioTheme: RadioThemeData(fillColor: WidgetStatePropertyAll(Color(0x80F5F6FA))),
+      textTheme: TextTheme(
+        labelLarge: TextStyle(color: Color(0xFFF5F6FA)),
+        labelMedium: TextStyle(color: Color(0xFFF5F6FA)),
+        labelSmall: TextStyle(color: Color(0xFFF5F6FA)),
+      ),
+      cardTheme: CardThemeData(
+        elevation: 4.0,
+        color: Color(0xFF28282B),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      ),
+      searchBarTheme: SearchBarThemeData(
+        elevation: WidgetStatePropertyAll(8.0),
+        backgroundColor: WidgetStatePropertyAll(Color(0xFF28282B)),
+        hintStyle: WidgetStatePropertyAll(TextStyle(color: Color(0xFFF5F6FA))),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        ),
+      ),
+      iconButtonTheme: IconButtonThemeData(
+        style: ButtonStyle(
+          elevation: WidgetStatePropertyAll(16.0),
+          backgroundColor: WidgetStatePropertyAll(Color(0xFF28282B)),
+          shape: WidgetStatePropertyAll(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          ),
+        ),
+      ),
+      chipTheme: ChipThemeData(
+        elevation: 4.0,
+        selectedColor: Color(0xFF00CEC9),
+        backgroundColor: Color(0x8000CEC9),
+        checkmarkColor: Color(0xFFFCFBFB),
+        side: BorderSide(color: Color(0xFF006462), width: 1.5),
+      ),
+      bottomSheetTheme: BottomSheetThemeData(
+        elevation: 12.0,
+        showDragHandle: true,
+        modalBackgroundColor: Color(0xFF28282B),
+      ),
+    );
+  }
+}
---
+++ lib/core/themes/light_mode.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LightMode {
+  static ThemeData initialize() {
+    return ThemeData(
+      useMaterial3: true,
+      dividerColor: Colors.transparent,
+      scaffoldBackgroundColor: Color(0xFFF5F6FA),
+      iconTheme: IconThemeData(color: Colors.grey.shade700),
+      radioTheme: RadioThemeData(fillColor: WidgetStatePropertyAll(Colors.grey)),
+      textTheme: TextTheme(
+        labelLarge: TextStyle(color: Colors.grey.shade800),
+        labelMedium: TextStyle(color: Colors.grey.shade800),
+        labelSmall: TextStyle(color: Colors.grey.shade800),
+      ),
+      cardTheme: CardThemeData(
+        elevation: 2.0,
+        color: Color(0xFFFCFBFB),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      ),
+      searchBarTheme: SearchBarThemeData(
+        elevation: WidgetStatePropertyAll(4.0),
+        backgroundColor: WidgetStatePropertyAll(Color(0xFFFCFBFB)),
+        hintStyle: WidgetStatePropertyAll(TextStyle(color: Colors.grey.shade700)),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        ),
+      ),
+      iconButtonTheme: IconButtonThemeData(
+        style: ButtonStyle(
+          elevation: WidgetStatePropertyAll(8.0),
+          backgroundColor: WidgetStatePropertyAll(Color(0xFFFCFBFB)),
+          shape: WidgetStatePropertyAll(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          ),
+        ),
+      ),
+      chipTheme: ChipThemeData(
+        elevation: 4.0,
+        selectedColor: Color(0xFF00CEC9),
+        backgroundColor: Color(0x8000CEC9),
+        checkmarkColor: Color(0xFFFCFBFB),
+        side: BorderSide(color: Color(0xFF006462), width: 1.5),
+      ),
+      bottomSheetTheme: BottomSheetThemeData(
+        elevation: 8.0,
+        showDragHandle: true,
+        modalBackgroundColor: Color(0xFFFCFBFB),
+      ),
+    );
+  }
+}
