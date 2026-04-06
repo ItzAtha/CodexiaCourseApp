c5bc4a40cb28e697421ff78b7383c4ca0750b229
ItzAtha <atha.06dha@gmail.com>
2026-04-06 18:55:21 +0700

    Add UserProfilePage for editing user profile information

---
+++ lib/features/profile/view/user_profile.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../shared/models/auth_user.dart';
+import '../../../shared/providers/auth_user_notifier.dart';
+
+class UserProfilePage extends ConsumerStatefulWidget {
+  const UserProfilePage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _UserProfilePageState();
+}
+
+class _UserProfilePageState extends ConsumerState<UserProfilePage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController displayNameController = TextEditingController();
+  final GlobalKey<FormFieldState> emailFieldKey = GlobalKey<FormFieldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    final authUserState = ref.read(authUserProvider);
+    AuthUser? authUser = authUserState.value;
+
+    if (authUser != null) {
+      emailController.text = authUser.email;
+      displayNameController.text = authUser.displayName ?? "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final authUserState = ref.watch(authUserProvider);
+    AuthUser? authUser = authUserState.value;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Profile"),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back),
+          style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.transparent)),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0x990984E3), Color(0xFF0984E3)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 40.0),
+            ClipOval(
+              child: Image.network(
+                authUser?.avatar?.avatarPath ??
+                    "https://cdn-icons-png.flaticon.com/128/3135/3135715.png",
+                width: 110.0,
+                height: 110.0,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  }
+
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Color(0xFF00CEC9),
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes!
+                          : null,
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "DisplayName",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).textTheme.labelSmall?.color,
+                  ),
+                ),
+                TextField(
+                  controller: displayNameController,
+                  style: TextStyle(
+                    color: Theme.of(context).textTheme.labelSmall?.color?.withValues(alpha: 0.9),
+                  ),
+                  decoration: InputDecoration(border: OutlineInputBorder(), isDense: true),
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  "Email Address",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).textTheme.labelSmall?.color,
+                  ),
+                ),
+                TextFormField(
+                  key: emailFieldKey,
+                  controller: emailController,
+                  readOnly: true,
+                  style: TextStyle(
+                    color: Theme.of(context).textTheme.labelSmall?.color?.withValues(alpha: 0.9),
+                  ),
+                  decoration: InputDecoration(
+                    hintText: "example@gmail.com",
+                    border: OutlineInputBorder(),
+                    isDense: true,
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+
+                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                      return 'Please enter a valid email address';
+                    }
+
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15.0),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                if (emailFieldKey.currentState?.validate() ?? false) {
+                  ref
+                      .read(authUserProvider.notifier)
+                      .updateDisplayName(
+                        displayNameController.text.isNotEmpty ? displayNameController.text : null,
+                      );
+                  Navigator.pop(context);
+                }
+              },
+              style: ButtonStyle(
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                ),
+                minimumSize: WidgetStatePropertyAll(Size(double.infinity, 40.0)),
+                backgroundColor: WidgetStatePropertyAll(Color(0xFF0984E3)),
+              ),
+              child: Text("Save", style: TextStyle(fontSize: 16.0, color: Colors.white)),
+            ),
+            SizedBox(height: 20.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
