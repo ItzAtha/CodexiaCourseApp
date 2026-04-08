380fb1ec1ca1cc85c335a96d9c15d54a2300a7ad
ItzAtha <atha.06dha@gmail.com>
2026-04-08 10:50:48 +0700

    Update Python course AppBar with gradient background and back button

--- lib/features/course/foundation/python/python_course.dart
+++ lib/features/course/foundation/python/python_course.dart
@@ -14,7 +14,25 @@ class _PythonCourseState extends State<PythonCourse> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Python Development"), centerTitle: true),
+      appBar: AppBar(
+        title: Text("Python Development"),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back),
+          style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.transparent)),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0x990984E3), Color(0xFF0984E3)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(20.0),
         child: Center(
