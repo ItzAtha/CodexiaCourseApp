6f4d65c8e5e6a0b0582b29c594f79bbdf1c23d6f
ItzAtha <atha.06dha@gmail.com>
2026-04-09 21:13:45 +0700

    Refactor authentication routing to use GoRouter and replace OpeningPage with AuthLandingPage

--- lib/features/auth/auth_router.dart
+++ lib/features/auth/auth_router.dart
@@ -1,59 +1,42 @@
-import 'package:codexia_course_learning/shared/providers/auth_user_notifier.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:codexia_course_learning/features/auth/widgets/auth_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:toastification/toastification.dart';
+import 'package:go_router/go_router.dart';
 
-import '../../core/utils/logger.dart';
-import 'views/opening_page.dart';
-import '../home/dashboard_page.dart';
+class AuthRouter {
+  AuthRouter._();
 
-class AuthenticationGate extends ConsumerWidget {
-  const AuthenticationGate({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    ref.listen(authUserProvider, (previous, next) {
-      next.when(
-        data: (data) {
-          DebugLogger(
-            message: "Successfully loaded profile: ${data.username}",
-            level: LogLevel.info,
-          ).log();
-        },
-        error: (error, stackTrace) {
-          DebugLogger(
-            message: "Error loading profile: $error",
-            stackTrace: stackTrace,
-            level: LogLevel.error,
-          ).log();
-          Toastification().show(
-            context: context,
-            title: Text("Couldn't load profile"),
-            type: ToastificationType.error,
-            style: ToastificationStyle.flat,
-            alignment: Alignment.bottomCenter,
-            autoCloseDuration: Duration(seconds: 2),
-            animationDuration: Duration(milliseconds: 500),
-          );
-        },
-        loading: () {
-          DebugLogger(message: "Loading profile...", level: LogLevel.info).log();
-        },
-      );
-    });
-
-    return Scaffold(
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const DashboardPage();
-          }
-
-          return OpeningPage();
-        },
-      ),
+  static GoRoute initialize() {
+    GoRoute authRouter = GoRoute(
+      path: '/auth',
+      name: 'authentication',
+      builder: (context, state) {
+        return AuthLandingPage();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'login',
+          name: 'login',
+          builder: (context, state) {
+            return const LoginPage();
+          },
+        ),
+        GoRoute(
+          path: 'register',
+          name: 'register',
+          builder: (context, state) {
+            return const RegisterPage();
+          },
+        ),
+        GoRoute(
+          path: 'reset-password',
+          name: 'reset-password',
+          builder: (BuildContext context, GoRouterState state) {
+            return const ResetPasswordPage();
+          },
+        ),
+      ],
     );
+
+    return authRouter;
   }
 }
