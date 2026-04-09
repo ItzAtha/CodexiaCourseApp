dd38a525508849a99517c80ff379b432e19be3d8
ItzAtha <atha.06dha@gmail.com>
2026-04-09 21:21:51 +0700

    Refactor DashboardPage to use StatefulNavigationShell for GoRouter and Riverpod for auth state management

--- lib/features/home/dashboard_page.dart
+++ lib/features/home/dashboard_page.dart
@@ -1,37 +1,43 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:animations/animations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:go_router/go_router.dart';
 import 'package:toastification/toastification.dart';
 
 import '../../core/utils/logger.dart';
-import './views/home_page.dart';
-import './views/course_page.dart';
-import './views/community_page.dart';
-import './views/setting_page.dart';
+import '../../shared/providers/auth_user_notifier.dart';
 import './models/bottom_navbar.dart';
 
-class DashboardPage extends StatefulWidget {
-  const DashboardPage({super.key});
+class DashboardPage extends ConsumerStatefulWidget {
+  const DashboardPage({super.key, required StatefulNavigationShell navigationShell})
+    : _navigationShell = navigationShell;
+
+  final StatefulNavigationShell _navigationShell;
 
   @override
-  State<StatefulWidget> createState() => _DashboardPageState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _DashboardPageState();
 }
 
-class _DashboardPageState extends State<DashboardPage> {
+class _DashboardPageState extends ConsumerState<DashboardPage> {
   bool isFloatingActionButtonOpened = false;
 
   int selectedIndex = 0;
   bool canCloseApp = false;
   DateTime? currentBackPressTime;
 
-  final List<Widget> pages = [HomePage(), CoursePage(), CommunityPage(), SettingPage()];
+  void _onItemTapped(int index) {
+    widget._navigationShell.goBranch(
+      index,
+      initialLocation: index == widget._navigationShell.currentIndex,
+    );
+  }
 
   void _onPopInvoked(bool canPop, Object? result) {
     if (selectedIndex != 0) {
       setState(() {
         selectedIndex = 0;
       });
+      widget._navigationShell.goBranch(selectedIndex);
       return;
     }
 
@@ -57,11 +63,42 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   Widget build(BuildContext context) {
+    ref.listen(authUserProvider, (previous, next) {
+      next.when(
+        data: (data) {
+          DebugLogger(
+            message: "Successfully loaded profile: ${data.username}",
+            level: LogLevel.info,
+          ).log();
+        },
+        error: (error, stackTrace) {
+          DebugLogger(
+            message: "Error loading profile: $error",
+            stackTrace: stackTrace,
+            level: LogLevel.error,
+          ).log();
+          Toastification().show(
+            context: context,
+            title: Text("Couldn't load profile"),
+            type: ToastificationType.error,
+            style: ToastificationStyle.flat,
+            alignment: Alignment.bottomCenter,
+            autoCloseDuration: Duration(seconds: 2),
+            animationDuration: Duration(milliseconds: 500),
+          );
+        },
+        loading: () {
+          DebugLogger(message: "Loading profile...", level: LogLevel.info).log();
+        },
+      );
+    });
+
     return PopScope(
       canPop: canCloseApp,
       onPopInvokedWithResult: (canPop, result) => _onPopInvoked(canPop, result),
       child: Scaffold(
         extendBody: true,
+        resizeToAvoidBottomInset: false,
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: SpeedDial(
           icon: Icons.add,
@@ -98,19 +135,11 @@ class _DashboardPageState extends State<DashboardPage> {
         bottomNavigationBar: BottomNavbar(
           onItemSelected: (index) {
             setState(() => selectedIndex = index);
+            _onItemTapped(selectedIndex);
           },
           onItemSelectedNotifier: ValueNotifier(selectedIndex),
         ),
-        body: PageTransitionSwitcher(
-          duration: const Duration(milliseconds: 500),
-          transitionBuilder: (child, primaryAnimation, secondaryAnimation) => FadeThroughTransition(
-            fillColor: Colors.transparent,
-            animation: primaryAnimation,
-            secondaryAnimation: secondaryAnimation,
-            child: child,
-          ),
-          child: pages[selectedIndex],
-        ),
+        body: widget._navigationShell,
       ),
     );
   }
