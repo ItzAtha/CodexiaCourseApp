da457ebc3b4bf267660dd6ac8330d52785529991
ItzAtha <atha.06dha@gmail.com>
2026-04-09 21:23:16 +0700

    Disable resizing for bottom insets in HomePage scaffold

--- lib/features/home/views/home_page.dart
+++ lib/features/home/views/home_page.dart
@@ -58,6 +58,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HomeAppBar(),
+      resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
