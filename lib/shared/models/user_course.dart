45cc5f5e85dfe56294eb9e4da974971837cf215e
ItzAtha <atha.06dha@gmail.com>
2026-04-17 20:59:19 +0700

    Update UserCourse and UserCourseList models with new fields and updated JSON serialization

--- lib/shared/models/user_course.dart
+++ lib/shared/models/user_course.dart
@@ -2,50 +2,62 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:codexia_course_learning/shared/enums/course_level.dart';
 
 class UserCourse {
+  final String courseId;
   final String courseName;
   final CourseLevel courseLevel;
   final double progress;
   final Timestamp startDate;
+  Timestamp? endDate;
 
   UserCourse({
+    required this.courseId,
     required this.courseName,
     required this.courseLevel,
     required this.progress,
     required this.startDate,
+    this.endDate,
   });
+
+  UserCourse.defaultCourse()
+    : courseId = '',
+      courseName = '',
+      courseLevel = CourseLevel.beginner,
+      progress = 0.0,
+      startDate = Timestamp.now();
 
   factory UserCourse.fromJson(Map<String, dynamic> json) {
     return UserCourse(
-      courseName: json['courseName'],
-      courseLevel: CourseLevel.values.firstWhere((level) => level.name == json['courseLevel']),
+      courseId: json['id'],
+      courseName: json['name'],
+      courseLevel: CourseLevel.values.firstWhere((level) => level.name == json['level']),
       progress: json['progress'].toDouble(),
       startDate: json['startDate'],
+      endDate: json['endDate'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'id': courseId,
       'courseName': courseName,
       'courseLevel': courseLevel.name,
       'progress': progress,
       'startDate': startDate.toDate(),
+      'endDate': endDate?.toDate(),
     };
   }
 }
 
 class UserCourseList {
-  final List<UserCourse> courses;
+  final List<UserCourse> courseList;
 
-  UserCourseList({required this.courses});
+  UserCourseList({required this.courseList});
 
-  factory UserCourseList.fromJson(Map<String, dynamic> json) {
-    List<dynamic> jsonSerialize = json['data'];
-    return UserCourseList(
-      courses: jsonSerialize.map((course) => UserCourse.fromJson(course)).toList(),
-    );
+  factory UserCourseList.fromJson(List<dynamic> json) {
+    return UserCourseList(courseList: json.map((course) => UserCourse.fromJson(course)).toList());
   }
 
   Map<String, dynamic> toJson() {
-    return {'data': courses.map((course) => course.toJson()).toList()};
+    return {'data': courseList.map((course) => course.toJson()).toList()};
   }
 }
