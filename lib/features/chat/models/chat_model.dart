aea70f49ff0f9e7dab5e314094e3db1eb384710c
ItzAtha <atha.06dha@gmail.com>
2026-04-21 12:52:22 +0700

    Add ChatType enum and implement ChatModelChannel class
    - Add `ChatType` enum with `userToModel` and `userToUser` variants
    - Implement `ChatModelChannel` to manage chat sessions with support for `chatId`, `title`, `lastUpdate`, and a list of `ChatModel` messages
    - Include JSON serialization and deserialization support for `ChatModelChannel`

--- lib/features/chat/models/chat_model.dart
+++ lib/features/chat/models/chat_model.dart
@@ -1,4 +1,5 @@
 enum Role { user, model }
+enum ChatType { userToModel, userToUser }
 
 class ChatModel {
   final String message;
@@ -17,5 +18,27 @@ class ChatModel {
 
   Map<String, dynamic> toJson() {
     return {'message': message, 'role': role.name, 'timestamp': timestamp.toIso8601String()};
+  }
+}
+
+class ChatModelChannel {
+  final String chatId;
+  final String? title;
+  final DateTime lastUpdate;
+  final List<ChatModel> messages;
+
+  ChatModelChannel({required this.chatId, this.title, required this.lastUpdate, required this.messages});
+
+  factory ChatModelChannel.fromJson(Map<String, dynamic> json) {
+    return ChatModelChannel(
+      chatId: json['chatId'],
+      title: json['title'],
+      lastUpdate: DateTime.parse(json['lastUpdate']),
+      messages: (json['messages'] as List).map((message) => ChatModel.fromJson(message)).toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'chatId': chatId, 'title': title, 'lastUpdate': lastUpdate.toIso8601String(), 'messages': messages.map((message) => message.toJson()).toList()};
   }
 }
