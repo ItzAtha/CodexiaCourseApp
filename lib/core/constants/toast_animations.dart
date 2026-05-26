937c3614043b63521b1cfecaaaab1ff230bc098d
ItzAtha <atha.06dha@gmail.com>
2026-05-26 18:43:55 +0700

    Initialize ToastAnimations to manage toast notification durations
    - Define `closeDuration` as 3 seconds for toast display time
    - Define `animationDuration` as 500 milliseconds for animation transitions
    - Implement a private constructor to prevent instantiation of the constants class

---
+++ lib/core/constants/toast_animations.dart
@@ -0,0 +1,6 @@
+class ToastAnimations {
+  ToastAnimations._();
+
+  static const Duration closeDuration = Duration(seconds: 3);
+  static const Duration animationDuration = Duration(milliseconds: 500);
+}
