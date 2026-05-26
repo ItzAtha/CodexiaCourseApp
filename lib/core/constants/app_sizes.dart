7fcd5a6c04a8b1f47d963384f96c09c4014eecc7
ItzAtha <atha.06dha@gmail.com>
2026-05-26 18:43:26 +0700

    Initialize AppSizes constant class to manage UI padding and text sizes
    - Define a set of padding constants ranging from 8.0 to 32.0 (`p8` through `p32`)
    - Define text size constants ranging from 12.0 to 28.0 (`sTextSize` through `xxlTextSize`)
    - Implement a private constructor to prevent instantiation of the utility class

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,17 @@
+class AppSizes {
+  AppSizes._();
+
+  // Padding size
+  static const double p8 = 8.0;
+  static const double p12 = 12.0;
+  static const double p16 = 16.0;
+  static const double p24 = 24.0;
+  static const double p32 = 32.0;
+
+  // Text size
+  static const double sTextSize = 12.0;
+  static const double mTextSize = 16.0;
+  static const double lTextSize = 20.0;
+  static const double xlTextSize = 24.0;
+  static const double xxlTextSize = 28.0;
+}
