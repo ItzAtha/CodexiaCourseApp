deb826e5b3fe094e9ad611d576f87a0311c47ac6
ItzAtha <atha.06dha@gmail.com>
2026-05-28 18:36:08 +0700

    Update CourseLevel enum to include a descriptive `name` field

--- lib/shared/enums/course_level.dart
+++ lib/shared/enums/course_level.dart
@@ -1,1 +1,10 @@
-enum CourseLevel { beginner, intermediate, expert, master }
+enum CourseLevel {
+  beginner("Beginner"),
+  intermediate("Intermediate"),
+  expert("Expert"),
+  master("Master");
+
+  final String name;
+
+  const CourseLevel(this.name);
+}
