e4820eaa97d566492cf25dc4ebeb57a231368af2
ItzAtha <atha.06dha@gmail.com>
2026-06-13 11:27:10 +0700

    Implement LevelProgressListConverter for UserLevelProgress serialization
    - Create `LevelProgressListConverter` to transform a JSON map of level data into a list of `UserLevelProgress` objects
    - Map JSON keys to the `levelName` field during deserialization in `fromJson`
    - Extract the `levelName` property to serve as the map key and remove it from the value object during serialization in `toJson`
    - Integrate with `freezed_annotation` to support custom mapping logic for nested progress data structures

---
+++ lib/services/level_progress_list_converter.dart
@@ -0,0 +1,27 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../shared/models/user_course_progress.dart';
+
+class LevelProgressListConverter implements JsonConverter<List<UserLevelProgress>, Map<String, dynamic>> {
+  const LevelProgressListConverter();
+
+  @override
+  List<UserLevelProgress> fromJson(Map<String, dynamic> json) {
+    return json.entries.map((entry) {
+      final Map<String, dynamic> data = Map<String, dynamic>.from(entry.value as Map);
+      data['levelName'] = entry.key;
+      return UserLevelProgress.fromJson(data);
+    }).toList();
+  }
+
+  @override
+  Map<String, dynamic> toJson(List<UserLevelProgress> list) {
+    final Map<String, dynamic> map = {};
+    for (var item in list) {
+      final data = item.toJson();
+      data.remove('levelName');
+      map[item.levelName] = data;
+    }
+    return map;
+  }
+}
