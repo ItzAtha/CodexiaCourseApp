379d1518024a97c149c3d24eb5fdbfab5a58be2f
ItzAtha <atha.06dha@gmail.com>
2026-06-15 16:59:26 +0700

    Refactor UserCourseProgress and UserLevelProgress models
    - Update `UserCourseProgress` to rename the `levelProgress` field to `levels` and transition to the `UserLevelProgressConverter`
    - Modify `UserLevelProgress` to replace `levelName` with `levelId` and remove the `totalModules` property
    - Add a `completedLesson` map to `UserLevelProgress` to track progress per module, mapped to the `modules` JSON key
    - Remove the `UserModuleProgress` class and the `fromEntry` and `toMap` helper methods from `UserLevelProgress`

--- lib/shared/models/user_course_progress.dart
+++ lib/shared/models/user_course_progress.dart
@@ -1,5 +1,6 @@
-import 'package:codexia_course_learning/services/level_progress_list_converter.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../services/user_level_progress_converter.dart';
 
 part 'user_course_progress.freezed.dart';
 
@@ -16,7 +17,10 @@ abstract class UserCourseProgress with _$UserCourseProgress {
     required String lastAccessedLesson,
     required DateTime lastAccessedAt,
 
-    @LevelProgressListConverter() @Default([]) List<UserLevelProgress> levelProgress,
+    @UserLevelProgressConverter()
+    @JsonKey(name: 'levels')
+    @Default([])
+    List<UserLevelProgress> levels,
   }) = _UserCourseProgress;
 
   factory UserCourseProgress.fromJson(Map<String, dynamic> json) =>
@@ -28,38 +32,12 @@ abstract class UserLevelProgress with _$UserLevelProgress {
   const UserLevelProgress._();
 
   const factory UserLevelProgress({
-    required String levelName,
+    required String levelId,
     required List<String> completedModules,
-    required int totalModules,
+
+    @JsonKey(name: 'modules') required Map<String, List<String>> completedLesson,
   }) = _UserLevelProgress;
 
   factory UserLevelProgress.fromJson(Map<String, dynamic> json) =>
       _$UserLevelProgressFromJson(json);
-
-  factory UserLevelProgress.fromEntry(MapEntry<String, dynamic> entry) {
-    final Map<String, dynamic> data = Map<String, dynamic>.from(entry.value as Map);
-    data['levelProgress'] = entry.key;
-    return UserLevelProgress.fromJson(data);
-  }
-
-  MapEntry<String, dynamic> toMap() {
-    final Map<String, dynamic> data = toJson();
-
-    data.remove('levelProgress');
-    return MapEntry(levelName, data);
-  }
-}
-
-@freezed
-abstract class UserModuleProgress with _$UserModuleProgress {
-  const UserModuleProgress._();
-
-  const factory UserModuleProgress({
-    required String lessonId,
-    required List<String> completedLessons,
-    required int totalLessons,
-  }) = _UserModuleProgress;
-
-  factory UserModuleProgress.fromJson(Map<String, dynamic> json) =>
-      _$UserModuleProgressFromJson(json);
 }
