8bdfeec9f73bf12d8a4027ed335551708a202720
ItzAtha <atha.06dha@gmail.com>
2026-07-08 13:40:55 +0700

    Refactor and rename ChatBubble to ChatMessageBubble
    - Rename `ChatBubble` to `ChatMessageBubble` and convert the class into a `StatelessWidget`
    - Replace the `isUser` boolean with a `Role` enum to distinguish between message sources
    - Move manual Markdown-like text parsing and formatting logic to separately class
    - Update widget styling to use `AppColors`, `AppSizes`, and standardized theme text styles
    - Configure dynamic `Card` elevation and background colors based on the message sender's role

---
+++ lib/features/chat/widgets/chat_message_bubble.dart
@@ -0,0 +1,29 @@
+import 'package:codexia_course_learning/core/app_constants.dart' show AppColors, AppSizes;
+import 'package:codexia_course_learning/features/chat/models/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessageBubble extends StatelessWidget {
+  final String message;
+  final Role role;
+
+  const ChatMessageBubble({super.key, required this.message, required this.role});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: role == Role.user ? 1.0 : 0.0,
+      clipBehavior: Clip.antiAlias,
+      color: role == Role.user
+          ? AppColors.secondary.withValues(alpha: 0.5)
+          : Theme.of(context).cardColor,
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: AppSizes.p8, horizontal: AppSizes.p12),
+        constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.8),
+        child: role == Role.user
+            ? Text(message, style: Theme.of(context).textTheme.labelLarge)
+            : Text(message, style: Theme.of(context).textTheme.labelLarge),
+      ),
+    );
+  }
+}
